f9d1ffbb80533bb412727359c105ab7836fd1f08
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-01 10:17:21 -0700

    Add schema for product component

---
+++ schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS product_component;
+
+CREATE DATABASE product_component;
+
+USE product_component;
+
+CREATE TABLE themes (
+  id INT NOT NULL AUTO_INCREMENT,
+  theme_name VARCHAR(255),
+  theme_image_url VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255),
+  price INT NOT NULL,
+  review_count INT NOT NULL,
+  rating INT NOT NULL,
+  theme_id INT NOT NULL,
+  featured VARCHAR(255),
+  choking_hazard BOOLEAN,
+  product_limit INT NOT NULL,
+  product_image_url VARCHAR(255),
+  PRIMARY KEY(id),
+  FOREIGN KEY (theme_id) REFERENCES themes(id)
+);
+
+CREATE TABLE stores (
+  id INT NOT NULL AUTO_INCREMENT,
+  store_name VARCHAR(255),
+  store_address VARCHAR(255),
+  product_id INT NOT NULL,
+  product_availability BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES products(id)
+);
