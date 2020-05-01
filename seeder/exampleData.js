66b9703a5ef9c4ebd7135ea2f5c3993e010539cb
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-01 15:53:35 -0700

    Add example data to populate entries

---
+++ seeder/exampleData.js
@@ -0,0 +1,96 @@
+const stores = [
+  {
+    name: 'The LEGO Store University Town Center',
+    address: '4545 La Jolla Village Dr University Town Center Space H23, San Diego, CA 92122'
+  },
+  {
+    name: 'The LEGO Store Ontario Mills',
+    address: '1 Mills Cir #631, Ontario, CA 91764',
+  },
+  {
+    name: 'The LEGO Store Fashion Valley',
+    address: 'Fashion Valley, 7007 Friars Rd Space 965A, San Diego, CA 92108',
+  },
+  {
+    name: 'LEGOLAND California Hotel LEGO Gift Shop',
+    address: '5885 The Crossings Dr, Carlsbad, CA 92008',
+  },
+  {
+    name: 'The LEGO Store NorthPark Center',
+    address: '8687 N Central Expy #770, Dallas, TX 75225',
+  },
+  {
+    name: 'The LEGO Store Arrowhead Towne Ctr',
+    address: 'Arrowhead Towne Ctr, 7700 W Arrowhead Towne Center Space 1109, Glendale, AZ 85308',
+  },
+  {
+    name: 'The LEGO Store Disneyland Resort',
+    address: 'Downtown Disney District, 1585 S, Disneyland Dr, Anaheim, CA 92802'
+  },
+  {
+    name: 'The LEGO Store South Coast Plaza',
+    address: '3333 Bristol Street South Coast Plaza Space 1042, Costa Mesa, CA 92626'
+  },
+  {
+    name: 'The LEGO Store Glendale Galleria',
+    address: '2130 Glendale Galleria, Glendale, CA 91210'
+  },
+  {
+    name: 'The LEGO Store Disney Springs',
+    address: '1672 E Buena Vista Dr Marketplace, Lake Buena Vista, FL 32830'
+  },
+  {
+    name: 'The LEGO Store Stoneridge',
+    address: '1444 Stoneridge Mall Space D117A, Pleasanton, CA 94588'
+  },
+  {
+    name: 'The LEGO Store Hillsdale Shopping Center',
+    address: '341 E Sailer Dr, San Mateo, CA 94403'
+  },
+  {
+    name: 'The LEGO Store Flatiron District',
+    address: '200 5th Ave, New York, NY 10010'
+  },
+  {
+    name: 'The LEGO Store Queens Ctr',
+    address: '90-15 Queens Blvd Suite 1076, Flushing, NY 11373'
+  },
+  {
+    name: 'The LEGO Store Mission Viejo',
+    address: '555 The, Shops At Mission Viejo Space 428B, Mission Viejo, CA 92691'
+  },
+  {
+    name: 'The LEGO Store San Francisco',
+    address: '865 Market St Space C41, San Francisco, CA 94103'
+  },
+  {
+    name: 'The LEGO Store Westfield Topanga',
+    address: '6600 Topanga Canyon Blvd, Canoga Park, CA 91303'
+  },
+  {
+    name: 'The LEGO Store Penn Square',
+    address: 'Penn Square Mall, 1901 Northwest Expy Space 1058B, Oklahoma City, OK 73118'
+  },
+  {
+    name: 'The LEGO Store Concord Mills',
+    address: '8111 Concord Mills Blvd, Mills #206, Concord, NC 28027'
+  },
+  {
+    name: 'The LEGO Store Annapolis',
+    address: '1260 Annapolis Mall Rd, Annapolis, MD 21401'
+  },
+];
+
+const themes = [
+  'LEGO Marvel', 'Star Wars', 'Architecture', 'City', 'Classic', 'DC Super Heroes', 'Harry Potter', 'LEGO Batman', 'Overwatch', 'NINJAGO'
+];
+
+const featured = ['New', 'Exclusives', 'Retiring soon', 'Hard to find'];
+
+
+
+module.exports = {
+  mockStores: stores,
+  mockThemes: themes,
+  mockFeatured: featured
+}
