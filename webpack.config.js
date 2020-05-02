c8f2cbe0522f3e6e4ad5a7cbf0b3454eba1f9a90
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-02 15:05:24 -0700

    Set up webpack/babel

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
