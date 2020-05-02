69456f438771a2165813c31c392b0673f4a4cdeb
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-01 17:15:18 -0700

    Refactor insertStores to use createStoreInventory

--- seeder/seedingScript.js
+++ seeder/seedingScript.js
@@ -1,8 +1,8 @@
 const {connection} = require('../server/db/index.js');
-const {createRandomStores, createRandomProducts} = require('./helpers.js');
+const {createStoreInventory, createRandomProducts} = require('./helpers.js');
 
 const insertStores = () => {
-  const storeList = createRandomStores(10);
+  const storeList = createStoreInventory(100);
   const query = 'INSERT INTO stores (store_name, store_address, product_id, product_availability) VALUES ?';
 
   connection.query(query, [storeList], (err, result) => {
@@ -14,7 +14,7 @@ const insertStores = () => {
 };
 
 const insertProducts = () => {
-  const productList = createRandomProducts(10);
+  const productList = createRandomProducts(100);
   const query = 'INSERT INTO products (product_name, price, review_count, rating, theme_name, theme_image_url, featured, choking_hazard, product_limit, product_image_url) VALUES ?';
 
   connection.query(query, [productList], (err, result) => {
