1f8e951d4093c99510f694a59ce7f1b5b9db993a
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-04 15:57:37 -0700

    Changed name of test file

---
+++ __tests__/ProductOverview.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProductOverview from '../client/src/components/ProductOverview.jsx';
+
+describe('Unit Tests', () => {
+  test('should render the app component on the screen', () => {
+    const wrapper = shallow(<ProductOverview />);
+    expect(wrapper).toExist();
+  });
+
+  test('should ')
+});
