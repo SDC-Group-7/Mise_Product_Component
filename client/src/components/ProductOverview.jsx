5b93e368eb5f8d5b7b0181939168024ad406ea1a
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-04 15:58:56 -0700

    Render product data onto DOM
    - Uses axios get request to fetch product data from database
    - Uses react hooks to set the state to the returned data
    - Passes in fetched data into components

--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReviewContainer from './ReviewContainer.jsx';
 import SafetyWarning from './SafetyWarning.jsx';
 import TabList from './TabList.jsx';
+import axios from 'axios';
 
 const ProductOverview = () => {
+  const [data, setData] = useState({});
+  const randomProductId = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + 1;
+
+  useEffect(() => {
+    const getProduct = async (id) => {
+      try {
+        const response = await axios.get(`http://localhost:3000/product/${id}`);
+        console.log(response.data[0]);
+        setData(response.data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProduct(randomProductId);
+  }, []);
+
   return (
     <div>
-      <span className="featured-badge">Hard to find</span>
-      <img src="https://legofec.s3-us-west-1.amazonaws.com/themes/HarryPotter.png" style={{display:"block", height:"auto", maxWidth:"100%", verticalAlign:"middle"}}></img>
+      <span className="featured-badge">{data.featured}</span>
+      <img src={data.theme_image_url} style={{display:"block", height:"auto", maxWidth:"100%", verticalAlign:"middle"}}></img>
       <h1 className="product-name">
-        <span>Hogwarts Castle</span>
+        <span>{data.product_name}</span>
       </h1>
       <ReviewContainer />
+      <h1>
+        <span>${data.price}</span>
+      </h1>
       <SafetyWarning />
       <TabList />
     </div>
