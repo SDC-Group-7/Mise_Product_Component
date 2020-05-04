a6eea6fc39c1b400ac1da204a6ffff3c43522315
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-04 11:15:14 -0700

    Move RevieContainer into own component

--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -1,56 +1,29 @@
 import React from 'react';
+import ReviewContainer from './ReviewContainer.jsx';
 
-const ProductOverview = () => (
-  <div>
-    <span className="featured-badge">Hard to find</span>
-    <img src="https://legofec.s3-us-west-1.amazonaws.com/themes/HarryPotter.png" style={{display:"block", height:"auto", maxWidth:"100%", verticalAlign:"middle"}}></img>
-    <h1 className="product-name">
-      <span>Hogwarts Castle</span>
-    </h1>
-    <div title="4.8" className="rating-review" style={{display:"flex", flexWrap:"wrap",}}>
-      <div type="stars" className="star" style={{width:"1.25rem", height:"1.25rem", margin:".38rem"}}>
-        <svg width="20" height="20" viewBox="0 0 50 49">
-          <g stroke="none" strokeWidth="1" style={{fill:"rgb(255, 213, 0)", alignItems:"center"}}>
-            <path d="M49.9 18.1 C 49.3 16.2 47.1 15.9 47.1 15.9 L 34 13.9 L 28 2 C 28 2 27 0 25 0 C 23 0 22 2 22 2 L 16.1 13.9 L 3 15.9 C 3 15.9 0.8 16.2 0.2 18.1 C -0.4 20 1.2 21.6 1.2 21.6 L 10.8 31 L 8.3 44 C 8.3 44 7.9 46.2 9.5 47.4 C 11.1 48.6 13.1 47.6 13.1 47.6 L 25 41.8 L 37.1 47.7 C 37.1 47.7 39.1 48.7 40.7 47.5 C 42.3 46.3 41.9 44.1 41.9 44.1 L 39.4 31.1 L 49 21.7 C 48.9 21.6 50.5 20 49.9 18.1 Z" fillRule="nonzero;">
-            </path>
-          </g>
-        </svg>
-      </div>
-      <div type="stars" className="star" style={{width:"1.25rem", height:"1.25rem", margin:".38rem"}}>
-        <svg width="20" height="20" viewBox="0 0 50 49">
-          <g stroke="none" strokeWidth="1" style={{fill:"rgb(255, 213, 0)"}}>
-            <path d="M49.9 18.1 C 49.3 16.2 47.1 15.9 47.1 15.9 L 34 13.9 L 28 2 C 28 2 27 0 25 0 C 23 0 22 2 22 2 L 16.1 13.9 L 3 15.9 C 3 15.9 0.8 16.2 0.2 18.1 C -0.4 20 1.2 21.6 1.2 21.6 L 10.8 31 L 8.3 44 C 8.3 44 7.9 46.2 9.5 47.4 C 11.1 48.6 13.1 47.6 13.1 47.6 L 25 41.8 L 37.1 47.7 C 37.1 47.7 39.1 48.7 40.7 47.5 C 42.3 46.3 41.9 44.1 41.9 44.1 L 39.4 31.1 L 49 21.7 C 48.9 21.6 50.5 20 49.9 18.1 Z" fillRule="nonzero;">
-            </path>
-          </g>
-        </svg>
-      </div>
-      <div type="stars" className="star" style={{width:"1.25rem", height:"1.25rem", margin:".38rem"}}>
-        <svg width="20" height="20" viewBox="0 0 50 49">
-          <g stroke="none" strokeWidth="1" style={{fill:"rgb(255, 213, 0)"}}>
-            <path d="M49.9 18.1 C 49.3 16.2 47.1 15.9 47.1 15.9 L 34 13.9 L 28 2 C 28 2 27 0 25 0 C 23 0 22 2 22 2 L 16.1 13.9 L 3 15.9 C 3 15.9 0.8 16.2 0.2 18.1 C -0.4 20 1.2 21.6 1.2 21.6 L 10.8 31 L 8.3 44 C 8.3 44 7.9 46.2 9.5 47.4 C 11.1 48.6 13.1 47.6 13.1 47.6 L 25 41.8 L 37.1 47.7 C 37.1 47.7 39.1 48.7 40.7 47.5 C 42.3 46.3 41.9 44.1 41.9 44.1 L 39.4 31.1 L 49 21.7 C 48.9 21.6 50.5 20 49.9 18.1 Z" fillRule="nonzero;">
-            </path>
-          </g>
-        </svg>
-      </div>
-      <div type="stars" className="star" style={{width:"1.25rem", height:"1.25rem", margin:".38rem"}}>
-        <svg width="20" height="20" viewBox="0 0 50 49">
-          <g stroke="none" strokeWidth="1" style={{fill:"rgb(255, 213, 0)"}}>
-            <path d="M49.9 18.1 C 49.3 16.2 47.1 15.9 47.1 15.9 L 34 13.9 L 28 2 C 28 2 27 0 25 0 C 23 0 22 2 22 2 L 16.1 13.9 L 3 15.9 C 3 15.9 0.8 16.2 0.2 18.1 C -0.4 20 1.2 21.6 1.2 21.6 L 10.8 31 L 8.3 44 C 8.3 44 7.9 46.2 9.5 47.4 C 11.1 48.6 13.1 47.6 13.1 47.6 L 25 41.8 L 37.1 47.7 C 37.1 47.7 39.1 48.7 40.7 47.5 C 42.3 46.3 41.9 44.1 41.9 44.1 L 39.4 31.1 L 49 21.7 C 48.9 21.6 50.5 20 49.9 18.1 Z" fillRule="nonzero;">
-            </path>
-          </g>
-        </svg>
-      </div>
-      <div type="stars" className="star" style={{width:"1.25rem", height:"1.25rem", margin:"0.38rem"}}>
-        <svg width="20" height="20" viewBox="0 0 50 49">
-          <g stroke="none" strokeWidth="1" style={{fill:"rgb(255, 213, 0)"}}>
-            <path d="M49.9 18.1 C 49.3 16.2 47.1 15.9 47.1 15.9 L 34 13.9 L 28 2 C 28 2 27 0 25 0 C 23 0 22 2 22 2 L 16.1 13.9 L 3 15.9 C 3 15.9 0.8 16.2 0.2 18.1 C -0.4 20 1.2 21.6 1.2 21.6 L 10.8 31 L 8.3 44 C 8.3 44 7.9 46.2 9.5 47.4 C 11.1 48.6 13.1 47.6 13.1 47.6 L 25 41.8 L 37.1 47.7 C 37.1 47.7 39.1 48.7 40.7 47.5 C 42.3 46.3 41.9 44.1 41.9 44.1 L 39.4 31.1 L 49 21.7 C 48.9 21.6 50.5 20 49.9 18.1 Z" fillRule="nonzero;">
-            </path>
-          </g>
-        </svg>
-      </div>
-    <button kind="ghost" type="submit" style={{display:"inline-block", borderColor:"transparent", borderRadius:"4px", borderCollapse:"collapse", color:"rgb(0, 109, 183)", fontWeight:"500", fontSize:"0.875rem", lineHeight:"1.1875rem", cursor:"pointer"}}>200 Reviews</button>
-    <button kind="ghost" type="submit" style={{display:"inline-block", borderColor:"transparent", borderRadius:"4px", borderCollapse:"collapse", color:"rgb(0, 109, 183)", fontWeight:"500", fontSize:"0.875rem", lineHeight:"1.1875rem", cursor:"pointer"}}>Submit Review</button>
+const ProductOverview = () => {
+  return (
+    <div>
+      <span className="featured-badge">Hard to find</span>
+      <img src="https://legofec.s3-us-west-1.amazonaws.com/themes/HarryPotter.png" style={{display:"block", height:"auto", maxWidth:"100%", verticalAlign:"middle"}}></img>
+      <h1 className="product-name">
+        <span>Hogwarts Castle</span>
+      </h1>
+      <ReviewContainer />
     </div>
+  )
+
+}
+
+
+
+
+  <div>
+
+
+
+
+
     <div className="safety-warning" style={{display:"flex", textAlign:"left", flexDirection:"row-reverse", alignItems:"center", justifyContent:"space-between", wordBreak:"break-word"}}>
       <span className="warning-message">Warning!<br/>Choking Hazard.<br/>Small parts.</span>
       <div className="warning-icon" display="block">This is a warning icon.</div>
@@ -78,6 +51,6 @@ const ProductOverview = () => (
       </div>
     </div>
   </div>
-);
+
 
 export default ProductOverview;
