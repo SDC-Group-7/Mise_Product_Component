27d72d1fe808a53075b4abd1e8c9159d3bbf1b92
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-06 00:35:20 -0700

    Move getProduct out of ProductOverview component

--- __tests__/ProductOverview.test.jsx
+++ __tests__/ProductOverview.test.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ProductOverview from '../client/src/components/ProductOverview';
+import axios from 'axios';
+import
+
+jest.mock('axios')
 
 describe('Unit Tests', () => {
   test('should render the app component on the screen', () => {
     const wrapper = shallow(<ProductOverview />);
     expect(wrapper).toExist();
   });
+});
+
+describe('Data fetcher', () => {
+  test('should fetch data from database', async () => {
+
+    expect(
+  })
 });
--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -4,44 +4,57 @@ import ReviewContainer from './ReviewContainer';
 import SafetyWarning from './SafetyWarning';
 import TabList from './TabList';
 
+const getProduct = async (id) => {
+  try {
+    return await axios.get(`http://localhost:3000/product/${id}`);
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
 const ProductOverview = () => {
-  const [data, setData] = useState({});
+  const [productData, setProductData] = useState({});
   const randomProductId = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1) + 1)) + 1;
+  const {
+    featured,
+    themeImageUrl,
+    productName,
+    price,
+    chokingHazard,
+    reviewCount,
+    rating,
+  } = productData;
 
   useEffect(() => {
-    const getProduct = async (id) => {
-      try {
-        const response = await axios.get(`http://localhost:3000/product/${id}`);
-        console.log(response.data[0]);
-        setData(response.data[0]);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getProduct(randomProductId);
+    getProduct(randomProductId)
+      .then(({ data }) => {
+        setProductData(data[0]);
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
+
     <div>
-      <span className="featured-badge">{data.featured}</span>
+      <span className="featured-badge">{featured}</span>
       <img
-        src={data.theme_image_url}
+        src={themeImageUrl}
         alt=""
         style={{
           display: 'block', height: 'auto', maxWidth: '100%', verticalAlign: 'middle',
         }}
       />
       <h1 className="product-name">
-        <span>{data.product_name}</span>
+        <span>{productName}</span>
       </h1>
-      <ReviewContainer />
+      <ReviewContainer reviewCount={reviewCount} rating={rating} />
       <h1>
         <span>
           $
-          {data.price}
+          {price}
         </span>
       </h1>
-      {data.choking_hazard ? <SafetyWarning /> : null}
+      {chokingHazard ? <SafetyWarning /> : null}
       <TabList />
     </div>
   );
