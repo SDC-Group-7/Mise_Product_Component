710197dab47361262a9a5aadda29b2e087c2825f
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-06 23:22:49 -0700

    Add functionality to tab buttons
    - User able to switch betwen buy now tab and check store tab

--- client/src/components/TabList.jsx
+++ client/src/components/TabList.jsx
@@ -1,45 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
+import styled from 'styled-components';
 import BuyNowTab from './BuyNowTab';
 import CheckStoreTab from './CheckStoreTab';
 
-const TabList = ({ productLimit, productAvailabilityOnline, themeName }) => (
-  <div>
-    <ul
-      className="tab-list"
-      style={{
-        display: 'flex', alignItems: 'stretch', width: '100%', padding: '0px', margin: '0px',
-      }}
-    >
-      <li className="tab" style={{ justifyContent: 'flex-start', width: 'auto', listStyleType: 'none' }}>
-        <button
-          type="submit"
-          style={{
-            background: 'transparent', boxShadow: 'none', cursor: 'pointer', borderStyle: 'initial',
-          }}
-        >
-          Buy Now
-        </button>
-      </li>
-      <li className="tab" style={{ justifyContent: 'flex-start', width: 'auto', listStyleType: 'none' }}>
-        <button
-          type="submit"
-          style={{
-            background: 'transparent', boxShadow: 'none', cursor: 'pointer', borderStyle: 'initial',
-          }}
-        >
-          Check Store Stock
-        </button>
-      </li>
-    </ul>
-    <BuyNowTab
-      productLimit={productLimit}
-      productAvailabilityOnline={productAvailabilityOnline}
-      themeName={themeName}
-    />
-    <CheckStoreTab />
-  </div>
-);
+const TabList = ({ productLimit, productAvailabilityOnline, themeName }) => {
+  const [tab, setTab] = useState(true);
+  const handleClick = () => setTab(!tab);
+
+  return (
+    <div>
+      <Tabs>
+        <Tab>
+          <TabButton className="BuyNow" onClick={handleClick}>Buy Now</TabButton>
+        </Tab>
+        <Tab>
+          <TabButton className="CheckStore" onClick={handleClick}>Check Store Stock</TabButton>
+        </Tab>
+      </Tabs>
+      {tab
+        ? (
+          <BuyNowTab
+            productLimit={productLimit}
+            productAvailabilityOnline={productAvailabilityOnline}
+            themeName={themeName}
+          />
+        )
+        : <CheckStoreTab />}
+    </div>
+  );
+};
+
+const Tabs = styled.ul`
+  display: flex;
+  align-items: stretch;
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+`;
+
+const Tab = styled.li`
+  justifyContent: flex-start;
+  width: auto;
+  list-style-type: none;
+`;
+
+const TabButton = styled.button`
+  background: transparent;
+  box-shadow: none;
+  cursor: pointer;
+  border-style: initial;
+`;
 
 TabList.propTypes = {
   productLimit: PropTypes.number,
