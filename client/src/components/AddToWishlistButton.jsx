fb9143aa0d0c9b6d70196f608cd7cd612dfb88bd
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-07 19:56:14 -0700

    Create basic button component

---
+++ client/src/components/AddToWishlistButton.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const AddToWishListButton = () => (
+  <WishListButton />
+);
+
+export default AddToWishListButton;
+
+const WishListButton = styled.button`
+
+`;
