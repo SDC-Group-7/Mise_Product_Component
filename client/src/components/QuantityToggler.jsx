c37160aadd83c5b4b4e4b154b4cab1aaf9672499
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-07 21:19:23 -0700

    Create basic toggle functionality

--- client/src/components/QuantityToggler.jsx
+++ client/src/components/QuantityToggler.jsx
@@ -1,5 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
 const QuantityToggler = () => (
-  'This is a quantity bar'
+  <Container>
+    <LeftButton />
+    <Input value={1} />
+    <RightButton />
+  </Container>
 );
 
 export default QuantityToggler;
+
+const Container = styled.div`
+  display: flex;
+`;
+const Input = styled.input``;
+const LeftButton = styled.button``;
+const RightButton = styled.button``;
