49827971ccf9648edbff9e313e55d2a39a47b2dd
Joey Nguyen <joeylnguyen95@gmail.com>
2020-05-17 17:36:22 -0700

    Configure webpack for dev and prod environments

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebPackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  plugins: [
+    new CleanWebPackPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Production',
+      template: 'template.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
-const zlib = require('zlib');
+// const path = require('path');
+// const CompressionPlugin = require('compression-webpack-plugin');
+// const zlib = require('zlib');
 
-module.exports = {
-  mode: 'production',
-  entry: './client/src/index.jsx',
-  plugins: [
-    new CompressionPlugin({
-      filename: '[path].br[query]',
-      algorithm: 'brotliCompress',
-      test: /\.(js|css|html|svg)$/,
-      compressionOptions: {
-        level: 11,
-      },
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: false,
-    }),
-    new CompressionPlugin({
-      algorithm: 'gzip',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-};
+// module.exports = {
+//   mode: 'production',
+//   entry: './client/src/index.jsx',
+//   plugins: [
+//     new CompressionPlugin({
+//       filename: '[path].br[query]',
+//       algorithm: 'brotliCompress',
+//       test: /\.(js|css|html|svg)$/,
+//       compressionOptions: {
+//         level: 11,
+//       },
+//       threshold: 10240,
+//       minRatio: 0.8,
+//       deleteOriginalAssets: false,
+//     }),
+//     new CompressionPlugin({
+//       algorithm: 'gzip',
+//     }),
+//   ],
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.join(__dirname, 'public'),
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: 'babel-loader',
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+// };
