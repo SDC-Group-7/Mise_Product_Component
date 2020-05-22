9b1982f4643ba781dc936af06809efb0304ccdb3
Akshaj Mody <akshajmody@gmail.com>
2020-05-22 11:16:49 -0700

    Schemas pending approval

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/target', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/product_component', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
@@ -8,20 +8,23 @@ db.once('open', function() {
   console.log('CONNECTED TO MONGO DATABASE')
 });
 
-// /products/:id
-const productSchema = mongoose.Schema({
+// PRODUCT SCHEMA------------------------------------------------------
+const ProductSchema = mongoose.Schema({
   productName: String,
   price: Number,
   reviewCount: Number,
   rating: Number,
   themeName: String,
   themeImageUrl: String,
+  featured: String,
+  chokingHazard: Boolean,
+  productLimit: Number,
+  productAvailabilityOnline: Boolean,
 });
 
+const Product = mongoose.model('Products', ProductSchema);
 
-const Product = mongoose.model('Products', productSchema);
-
-const save = (productData, callback) => {
+const saveProduct = (productData, callback) => {
   const productDocs = [];
 
   productData.forEach((asource) => {
@@ -51,11 +54,74 @@ const findAllProducts = (callback) => {
 
 const findProduct = (id, callback) => Product.findOne({ productId: id }, callback);
 
-const drop = () => {
+const dropProduct = () => {
   Product.collection.drop();
 };
 
-module.exports.findAllProducts = findAllProducts;
-module.exports.findProduct = findProduct;
-module.exports.save = save;
-module.exports.drop = drop;
+// STORE SCHEMA------------------------------------------------------
+
+const StoreSchema = mongoose.Schema({
+  productName: String,
+  price: Number,
+  reviewCount: Number,
+  rating: Number,
+  themeName: String,
+  themeImageUrl: String,
+  featured: String,
+  chokingHazard: Boolean,
+  productLimit: Number,
+  productAvailabilityOnline: Boolean,
+});
+
+const Store = mongoose.model('Stores', StoreSchema);
+
+const saveStore = (storeData, callback) => {
+  const storeDocs = [];
+
+  storeData.forEach((asource) => {
+    let oneStore = new Store();
+    let newDoc = Object.assign(oneStore, asource);
+    storeDocs.push(newDoc);
+  });
+
+  Store.insertMany(storeDocs, (err, docs) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, docs);
+    }
+  });
+};
+
+const findAllStores = (callback) => {
+  Store.find({}).exec((err, allStores) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, allStores);
+    }
+  });
+};
+
+const findStore = (id, callback) => Store.findOne({ storeId: id }, callback);
+
+const dropStore = () => {
+  Store.collection.drop();
+};
+
+
+// module.exports.findAllProducts = findAllProducts;
+// module.exports.findProduct = findProduct;
+// module.exports.saveProduct = saveProduct;
+// module.exports.dropProduct = dropProduct;
+
+module.exports = {
+  findAllProducts,
+  findProduct,
+  saveProduct,
+  dropProduct,
+  findAllStores,
+  findStore,
+  saveStore,
+  dropStore,
+};
